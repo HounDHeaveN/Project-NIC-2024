a74e16223ba7cfd133b945fd8286f11d7709d04e
Titu Ray choudhury <tituraychoudhury@gmail.com>
2024-06-21 14:05:18 +0530

    Implement Swagger for API Documentation

--- Project-NIC/src/main/java/com/projectapi/Project_NIC/ProjectNicApplication.java
+++ Project-NIC/src/main/java/com/projectapi/Project_NIC/ProjectNicApplication.java
@@ -2,8 +2,10 @@ package com.projectapi.Project_NIC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class ProjectNicApplication {
 
 	public static void main(String[] args) {
